f5879987e7080e80defbb398ad3a7b4677083575
mirna-k <mirna_krpic@hotmail.com>
2024-08-16 13:48:39 +0200

    Set base app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from './pages/login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
-export default App
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
